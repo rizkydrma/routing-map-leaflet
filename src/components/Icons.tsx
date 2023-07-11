e6803bf3b3517b9af50a088068e73a501418fd07
rizkydrma <rizky.darmarazak@gmail.com>
2023-07-11 16:13:45 +0700

    feat: create used Icons

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -1,9 +1,11 @@
-import { XCircleIcon, PlusCircleIcon, MapPin } from 'lucide-react';
+import { XCircleIcon, PlusCircleIcon, MapPin, ChevronRight, Loader2 } from 'lucide-react';
 
 export const Icons = {
   XCircleIcon,
   PlusCircleIcon,
   MapPin,
+  ChevronRight,
+  Loader2,
 };
 
 export default Icons;
