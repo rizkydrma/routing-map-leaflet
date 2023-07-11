20e75b72bde89b6ac8cd4f0812b9e44ee13c1457
rizkydrma <rizky.darmarazak@gmail.com>
2023-07-11 15:23:16 +0700

    fix: sidebar scroll y

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,6 @@
 import clsx from 'clsx';
-import { ChevronRight } from 'lucide-react';
 import { FC, HTMLAttributes } from 'react';
+import Icons from './Icons';
 
 interface SidebarProps extends HTMLAttributes<HTMLDivElement> {
   collapse: boolean;
@@ -10,21 +10,26 @@ interface SidebarProps extends HTMLAttributes<HTMLDivElement> {
 
 const Sidebar: FC<SidebarProps> = ({ collapse, setCollapse, children }) => {
   return (
-    <div
-      className={`bg-white border-r absolute shadow-lg shadow-black/50 z-20 h-full  transition duration-600 w-96 ${
-        collapse ? 'translate-x-0' : '-translate-x-96'
-      }`}
-    >
-      {children}
-
-      {/* <div className="h-[6px] w-[6px] rounded-full bg-gray-700 before:h-[6px] before:w-[6px] before:rounded-full before:bg-gray-700 before:-mt-3 before:absolute after:h-[6px] after:w-[6px] after:rounded-full after:bg-gray-700 after:mt-3 after:absolute"></div> */}
-
+    <div className="absolute">
+      <div
+        className={`bg-white border-r shadow z-20 h-full max-h-[100vh] transition duration-600 w-96 overflow-y-scroll ${
+          collapse ? 'translate-x-0' : '-translate-x-96'
+        }`}
+      >
+        {children}
+      </div>
       {/* BUTTON COLLAPSE */}
       <button
         onClick={() => setCollapse(!collapse)}
-        className="h-20 w-8 bg-white rounded-tr-md rounded-br-md absolute translate-x-96 top-0 bottom-0 translate-y-[calc(50vh)] border-l z-0 shadow-md items-center flex justify-center"
+        className={clsx(
+          'h-20 w-8 bg-white rounded-tr-md rounded-br-md absolute top-0 bottom-0 translate-y-[calc(50vh)] border-l z-10 shadow-md items-center flex justify-center transition duration-200',
+          collapse ? 'translate-x-96' : 'translate-x-0',
+        )}
       >
-        <ChevronRight size="22" className={clsx('transition duration-1000', collapse ? 'rotate-180' : 'rotate-0')} />
+        <Icons.ChevronRight
+          size="22"
+          className={clsx('transition duration-1000', collapse ? 'rotate-180' : 'rotate-0')}
+        />
       </button>
     </div>
   );
