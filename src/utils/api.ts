39508978a302f16ab12cfa0025ff874727e503d3
rizkydrma <rizky.darmarazak@gmail.com>
2023-07-12 16:26:56 +0700

    feat: add return type for API

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,14 +1,20 @@
+import { Geocoder } from '../types/geocoder';
+import { LocationItem } from '../types/location';
+
 const api = (() => {
   const BASE_URL_NOMINATIM = 'https://nominatim.openstreetmap.org';
 
-  async function getLocations(query: string) {
+  async function getLocations(query: string): Promise<LocationItem[]> {
     const response = await fetch(`${BASE_URL_NOMINATIM}/search?format=json&limit=5&q=${query}`);
     const responseJson = await response.json();
 
     return responseJson;
   }
 
-  async function getReverseGeocoding({ latitude, longitude }: Omit<Coordinates, 'id'>) {
+  async function getReverseGeocoding({
+    latitude,
+    longitude,
+  }: Pick<Coordinates, 'latitude' | 'longitude'>): Promise<Geocoder> {
     const response = await fetch(`${BASE_URL_NOMINATIM}/reverse?lat=${latitude}&lon=${longitude}&format=json`);
     const responseJson = await response.json();
 
