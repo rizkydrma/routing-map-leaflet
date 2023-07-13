e0e7495e64638c9963a660b09f90ffd5fcea2361
rizkydrma <rizky.darmarazak@gmail.com>
2023-07-13 13:30:06 +0700

    feat: add asset

---
+++ src/assets/index.ts
@@ -0,0 +1,5 @@
+import MarkerDefault from './marker-default.png';
+import MarkerStart from './marker-start.png';
+import MarkerFinish from './marker-finish.png';
+
+export { MarkerDefault, MarkerStart, MarkerFinish };
