086b3aece677af2156a63df61df10ce5a62a0b85
rizkydrma <rizky.darmarazak@gmail.com>
2023-07-13 13:30:28 +0700

    --

--- src/components/Routing.tsx
+++ src/components/Routing.tsx
@@ -3,6 +3,7 @@ import 'leaflet-routing-machine';
 import 'leaflet-routing-machine/dist/leaflet-routing-machine.css';
 import { FC, useEffect } from 'react';
 import { useMap, useMapEvents } from 'react-leaflet';
+import { MarkerDefault, MarkerFinish, MarkerStart } from '../assets';
 
 interface RoutingProps {
   geolocation: Coordinates[];
@@ -10,16 +11,11 @@ interface RoutingProps {
   handleClickMap: (e: L.LeafletMouseEvent) => void;
 }
 
-L.Marker.prototype.options.icon = L.icon({
-  iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
-});
-
 const Routing: FC<RoutingProps> = ({ geolocation, handleClickMap }) => {
   const map = useMap();
   useMapEvents({
     async click(e) {
       handleClickMap(e);
-
       map.locate();
     },
   });
@@ -31,21 +27,51 @@ const Routing: FC<RoutingProps> = ({ geolocation, handleClickMap }) => {
       .filter((location) => location?.latitude !== 0 && location?.longitude !== 0)
       .map((location) => L.latLng(location?.latitude, location?.longitude));
 
+    const plan = L.Routing.plan(wayPoints, {
+      createMarker: (wayPointsIndex: number, wayPoints: L.Routing.Waypoint, numberOfWayPoints: number) => {
+        let marker_icon = L.icon({
+          iconUrl: MarkerDefault,
+          iconSize: [40, 40],
+        });
+        if (wayPointsIndex == 0) {
+          marker_icon = L.icon({
+            iconUrl: MarkerStart,
+            iconSize: [40, 40],
+          });
+        } else if (wayPointsIndex == numberOfWayPoints - 1) {
+          marker_icon = L.icon({
+            iconUrl: MarkerFinish,
+            iconSize: [40, 40],
+          });
+        }
+        var marker = L.marker(wayPoints.latLng, {
+          draggable: true,
+          icon: marker_icon,
+        });
+
+        marker.on('dragend', (e) => {
+          console.log('end', e);
+        });
+        return marker;
+      },
+    });
+
     const routingControl = L.Routing.control({
-      // router: L.Routing.osrmv1({
-      //   serviceUrl: `http://router.project-osrm.org/route/v1/`,
-      // }),
       routeWhileDragging: true,
-      // showAlternatives: true,
+      addWaypoints: false,
       lineOptions: {
-        styles: [{ color: '#6FA1EC', weight: 4, opacity: 1 }],
+        extendToWaypoints: false,
+        missingRouteTolerance: 0,
+        styles: [{ color: '#2563eb', weight: 6, opacity: 0.8 }],
       },
       waypoints: wayPoints,
-      show: true,
-      addWaypoints: true,
-      fitSelectedRoutes: true,
-      collapsible: true,
+      show: false,
+      plan: plan,
     }).addTo(map);
+
+    routingControl.on('routeselected', (e) => {
+      console.log('eee', e);
+    });
 
     return () => {
       map.removeControl(routingControl);
